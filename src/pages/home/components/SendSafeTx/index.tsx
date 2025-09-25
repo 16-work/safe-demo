c469585fbf4062943c4409a401031622dd67aac1
soup16 <soup16-work@outlook.com>
2025-09-25 18:41:23 +0800

    feat: send token

--- src/pages/home/components/SendSafeTx/index.tsx
+++ src/pages/home/components/SendSafeTx/index.tsx
@@ -1,6 +1,7 @@
 import { SafeTransaction } from '@safe-global/types-kit';
 import { SignExecuteSafeTx } from '../SignExecuteSafeTx';
 import { useSafe } from '@/hooks/useSafe';
+import { encodeFunctionData, erc20Abi, parseUnits } from 'viem';
 
 /** Component */
 export const SendSafeTx = () => {
@@ -11,6 +12,7 @@ export const SendSafeTx = () => {
   const state = useReactive({
     safeAddress: '',
     to: '',
+    tokenContract: '',
     value: '',
     threshold: 0,
     signatures: 0,
@@ -25,9 +27,40 @@ export const SendSafeTx = () => {
     const threshold = await protocolKit.getThreshold();
     state.threshold = threshold;
 
+    let transaction;
+
+    // 主币转账
+    if (!state.tokenContract) {
+      transaction = {
+        to: state.to,
+        value: BigNumber(state.value)
+          .times(10 ** 18)
+          .toString(),
+        data: '0x',
+      };
+    }
+    // 代币转账
+    else {
+      const decimals = await hooks.contract.read({
+        address: state.tokenContract as `0x${string}`,
+        abi: erc20Abi,
+        functionName: 'decimals',
+        args: [],
+      });
+
+      transaction = {
+        to: state.tokenContract,
+        value: '0',
+        data: encodeFunctionData({
+          abi: erc20Abi,
+          functionName: 'transfer',
+          args: [state.to as `0x${string}`, parseUnits(state.value, decimals)],
+        }),
+      };
+    }
+
     const safeTransaction = await protocolKit.createTransaction({
-      transactions: [{ to: state.to, value: state.value, data: '0x' }],
-      onlyCalls: true,
+      transactions: [transaction],
     });
 
     state.safeTransaction = safeTransaction;
@@ -48,6 +81,11 @@ export const SendSafeTx = () => {
 
             <div>
               To: <input type="text" value={state.to} onChange={(e) => (state.to = e.target.value)} className="input-1 w-full" />
+            </div>
+
+            <div>
+              Token Contract:{' '}
+              <input type="text" value={state.tokenContract} onChange={(e) => (state.tokenContract = e.target.value)} className="input-1 w-full" />
             </div>
 
             <div>
